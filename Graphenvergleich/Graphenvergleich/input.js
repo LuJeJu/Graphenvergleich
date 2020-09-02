efaeb663d515b420e30f78caee7b3bbdedf0b449
JudithRoskoden <j.roskoden@googlemail.com>
2020-09-02 11:29:09 +0200

    yfvdfgdsydfyb

--- Graphenvergleich/Graphenvergleich/input.js
+++ Graphenvergleich/Graphenvergleich/input.js
@@ -1,12 +1,20 @@
-const files = document.getElementById("files");
-                files.onchange = function(event) {
-                var file = event.target.files[0];
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                	var content = e.target.result;
-                    var bayes = JSON.parse(content);
-                    console.log(bayes);
+var bayes=0;
+//var wert=false;
+function read(){
+
+					const files = event.target.files;
+					for (var i=0; i<files.length; i++) {
+						var file = files[i];
+						var reader = new FileReader();
+						reader.onload = function(e) {
+							var content = e.target.result;
+							bayes = JSON.parse(content);
+							
+                            //console.dir(bayes);
+                            //wert=true;
+							//console.log(bayes.A.children); 
+						}
+                        reader.readAsText(file);
+                        
                     }
-                 reader.readAsText(file);
-                  	
                 }
